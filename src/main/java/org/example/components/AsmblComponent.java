a437ee82e5b6f173db6c0c539ee853d817b413d4
HatKid21 <dimno2007@gmail.com>
2026-02-14 16:49:56 +0300

    fix hologram intersecting through blocks

--- src/main/java/org/example/components/AsmblComponent.java
+++ src/main/java/org/example/components/AsmblComponent.java
@@ -4,7 +4,6 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityType;
-import net.minestom.server.entity.Player;
 import net.minestom.server.entity.metadata.display.BlockDisplayMeta;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
@@ -13,24 +12,32 @@ import org.example.CustomPlayer;
 import org.example.utils.Ray;
 import org.example.utils.Scheme;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AsmblComponent {
 
     private final CustomPlayer player;
 
-    private final List<Entity> prevHolo;
+    private final List<Entity> holo;
 
-    private Pos prevPos;
-    private Scheme prevScheme;
+    private Pos placePos;
+    private Scheme scheme;
+
+    private static final Pos[] directions = {
+            new Pos (0,1,0),
+            new Pos (1,0,0),
+            new Pos (0,0,1),
+            new Pos (-1,0,0),
+            new Pos (0,0,-1),
+            new Pos(0,-1,0)
+    };
+
 
     public AsmblComponent(CustomPlayer player){
         this.player = player;
-        prevHolo = new ArrayList<>();
-        prevPos = new Pos(0,0,0);
-        prevScheme = new Scheme();
+        holo = new ArrayList<>();
+        placePos = new Pos(0,0,0);
+        scheme = new Scheme();
     }
 
     public void drawHologram(){
@@ -41,32 +48,36 @@ public class AsmblComponent {
             if (blockPos == null){
                 return;
             }
-            blockPos = blockPos.add(0,1,0);
-            if (prevPos.equals(blockPos)) {
+            if (placePos.equals(blockPos)) {
                 return;
             }
 
             Scheme scheme = player.getSelectComponent().getScheme();
 
-            if (!prevScheme.equals(scheme)){
+            if (!this.scheme.equals(scheme)){
                 clearHolo();
-                createHoloBlocks(player.getInstance(),blockPos,scheme);
+                blockPos = createHoloBlocks(player.getInstance(),blockPos,scheme);
             } else{
-                teleportHoloBlocks(blockPos);
+                blockPos = teleportHoloBlocks(blockPos);
             }
 
-            prevPos = blockPos;
+            this.scheme = scheme;
+
+            placePos = blockPos;
         }
     }
 
-    private void teleportHoloBlocks(Pos blockPos){
-       for (Entity entity : prevHolo){
+    private Pos teleportHoloBlocks(Pos blockPos){
+        blockPos = findValidPos(blockPos);
+       for (Entity entity : holo){
            Pos relativePos = getRelativePos(entity);
            entity.teleport(blockPos.add(relativePos));
        }
+       return blockPos;
     }
 
-    private void createHoloBlocks(Instance instance, Pos blockPos,Scheme scheme){
+    private Pos createHoloBlocks(Instance instance, Pos blockPos,Scheme scheme){
+        blockPos = findValidPos(blockPos);
         for (Map.Entry<Pos, Block> entry : scheme.getBlocks().entrySet()){
             Block block = entry.getValue();
             Pos relativePos = entry.getKey();
@@ -84,24 +95,27 @@ public class AsmblComponent {
 
             entity.setInstance(instance,blockPos.add(relativePos));
 
-            prevHolo.add(entity);
+            holo.add(entity);
         }
+        return blockPos;
     }
 
 
     public void clearHolo(){
-        for (Entity entity : prevHolo){
+        for (Entity entity : holo){
             entity.remove();
         }
-        prevHolo.clear();
+        holo.clear();
+        placePos = new Pos(0,0,0);
+        scheme = new Scheme();
     }
 
     public void assembly(){
-        for (Entity entity : prevHolo){
+        for (Entity entity : holo){
             Pos relativePos = getRelativePos(entity);
             BlockDisplayMeta meta = (BlockDisplayMeta) entity.getEntityMeta();
             Block block = meta.getBlockStateId();
-            player.getInstance().setBlock(relativePos.add(prevPos),block);
+            player.getInstance().setBlock(relativePos.add(placePos),block);
         }
         clearHolo();
     }
@@ -111,6 +125,49 @@ public class AsmblComponent {
        int relativeY = entity.getTag(Tag.Integer("RelativeY"));
        int relativeZ = entity.getTag(Tag.Integer("RelativeZ"));
        return new Pos(relativeX,relativeY,relativeZ);
+    }
+
+    private Pos findValidPos(Pos startPos){
+        Queue<Pos> queue = new LinkedList<>();
+        Set<Pos> visited = new HashSet<>();
+
+        queue.add(startPos);
+        visited.add(startPos);
+
+        int maxIterations = 30;
+        int iteration = 0;
+
+        while (!queue.isEmpty() && iteration < maxIterations){
+            Pos curPos = queue.poll();
+            iteration++;
+
+            if (canPlace(curPos)){
+                return curPos;
+            }
+
+            for (Pos dir : directions){
+                Pos next = curPos.add(dir);
+
+                if (!visited.contains(next)){
+                    visited.add(next);
+                    queue.add(next);
+                }
+            }
+
+        }
+
+        return null;
+
+    }
+
+    private boolean canPlace(Pos pos){
+        for (Map.Entry<Pos,Block> entry : scheme.getBlocks().entrySet()){
+            Pos relativePos = entry.getKey();
+            if (!player.getInstance().getBlock(pos.add(relativePos)).isAir()){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
