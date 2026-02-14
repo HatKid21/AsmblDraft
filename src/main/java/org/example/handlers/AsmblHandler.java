68ecf53817570991b8648872f86503cfcdbecd59
HatKid21 <dimno2007@gmail.com>
2026-02-14 15:45:15 +0300

    fix select tool activation logic

--- src/main/java/org/example/handlers/AsmblHandler.java
+++ src/main/java/org/example/handlers/AsmblHandler.java
@@ -1,7 +1,9 @@
 package org.example.handlers;
 
-import net.kyori.adventure.text.Component;
+import net.minestom.server.entity.PlayerHand;
 import net.minestom.server.event.player.PlayerTickEvent;
+import net.minestom.server.event.player.PlayerUseItemEvent;
+import net.minestom.server.event.player.PlayerUseItemOnBlockEvent;
 import org.example.CustomPlayer;
 import org.example.components.AsmblComponent;
 import org.example.utils.Tools;
@@ -16,7 +18,15 @@ public class AsmblHandler extends AbstractHandler{
                 if (player.getItemInMainHand().equals(Tools.ASMBL_TOOL)){
                     AsmblComponent asmblComponent = player.getAsmblComponent();
                     asmblComponent.drawHologram();
+                }
+            }
+        });
 
+        globalEventHandler.addListener(PlayerUseItemEvent.class,event ->{
+            if (isOn){
+                final CustomPlayer player = (CustomPlayer) event.getPlayer();
+                if (event.getHand() == PlayerHand.MAIN && event.getItemStack().equals(Tools.ASMBL_TOOL)){
+                    //TODO
                 }
             }
         });
--- src/main/java/org/example/handlers/SelectHandler.java
+++ src/main/java/org/example/handlers/SelectHandler.java
@@ -5,13 +5,14 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.entity.PlayerHand;
 import net.minestom.server.event.player.PlayerUseItemOnBlockEvent;
 import org.example.CustomPlayer;
+import org.example.utils.Tools;
 
 public class SelectHandler extends AbstractHandler{
 
     public SelectHandler(){
         globalEventHandler.addListener(PlayerUseItemOnBlockEvent.class, event ->{
             if (isOn){
-                if (event.getHand() != PlayerHand.MAIN) {
+                if (event.getHand() == PlayerHand.MAIN && event.getItemStack().equals(Tools.SELECT_TOOL)) {
                     final CustomPlayer player = (CustomPlayer) event.getPlayer();
                     player.getSelectComponent().select(event.getPosition());
                 }
